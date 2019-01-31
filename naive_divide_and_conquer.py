a3d84995fb45f175ab8e2121dfe155efac46f66e
Alejandro Tovar <tovaral@flip1.engr.oregonstate.edu>
2019-01-30 21:56:42 -0800

    making progress, but new errors

--- naive_divide_and_conquer.py
+++ naive_divide_and_conquer.py
@@ -1,6 +1,7 @@
 from helpers import *
 from brute_force import *
 import sys
+import math
 
 def read_file(file):
 	points = []
@@ -16,46 +17,59 @@ def closest_pair(points):
 	if length <= 3:
 		return brute_force(points)
 	else:
-		M = []
-		sortedList = []
+		sortXarray = sortX(points)					#sort points by x
+		midx = median(sortXarray)					#get the median x(middle point)
+		print "mid", midx
 		left = []
 		right = []
-		midx = points[mid][0]
-		for x in points:
-			if x[0] <= midx:
+		for x in sortXarray:						#get all the points with x smaller than median on the
+			if x[0] <= midx:						#left, bigger on the right			
 				left.append(x)
 			else:
 				right.append(x)
-				
-		minlist1 = closest_pair(left)
-		minlist2 = closest_pair(right)
-		for i in range(length):
-			x = get_x(points[i])
-			if x < max and x > min:
-				M.append(points[i])
-		#sortedList = sortY(points)
-		print "left", minlist1
-		print "right", minlist2
+		leftArray = closest_pair(left)				#recursive call
+		rightArray = closest_pair(right)
+		minLeft = leftArray[0][0]					#get the min distance on left side
+		minRight = rightArray[0][0]					#get the min distance on right side
+		min = min_distance(minLeft, minRight)		#get the smaller distance
+		delta1 = min + midx							#get the range of middle set
+		delta2 = midx - min
+		merge_array = leftArray.append(rightArray)	#merge left and right arrays
+		M = []
+		for x in merge_array:						#get all points that fall in middle into M
+			if x[1] >= delta2 and x[1] <= delta1:
+				M.append(x)
+		sortedlist = sortY(M)						#sort them by Y in increasing order
+		print "left", left
+		print "right", right
+		print "left", left
+		print "right", right
 	return
 	
 def sortY(list):
-	length = len(list)
-	print length
-	for i in range(length):
-		min = list[i[1]]
-		pos = i
-		while pos < length and list[i+1[1]] > min:
-			list[i[1]] = list[i+1[1]]
-			pos = pos +1
-		list[pos[1]]=min
-	return list
+	return sorted(list , key=lambda k: [k[1], k[0]])
+def sortX(list):
+	return sorted(list, key=lambda k: k[0])
 		
-		
+def median(points):
+	length = len(points)
+	index = length//2
+	if length % 2:
+		return points[index][0]
+	else:
+		return (int(points[index][0]) + int(points[index + 1][0]))/2
 
 def main():
-	points_file = "Testing Cases/random_10_3.txt"
-	num, points = read_file(points_file)
-	min = closest_pair(points)
+	points = []
+	name = sys.argv[1]
+	with open(name, 'r') as filename:
+		for point in filename:
+			p1 = int(point.strip().split(' ')[0])
+			p2 = int(point.strip().split(' ')[1])
+			points.append((p1,p2))
+
+	minimum = closest_pair(points)
+	return minimum
 	
 	
 if __name__ == '__main__':
