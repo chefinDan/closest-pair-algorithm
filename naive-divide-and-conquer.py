868f7f865000d26d5406b1880f59a21d89044641
chefinDan <greendan@oregonstate.edu>
2019-02-02 13:58:27 -0800

    added naive method, comments, and updated readme with instructions for usage

--- naive-divide-and-conquer.py
+++ naive-divide-and-conquer.py
@@ -1,5 +1,4 @@
-# Author: Daniel Green, greendan@oregonstate.edu
-
+# Author: Daniel Green, Alex Lung
 import sys
 from sort_rands import *
 from bruteforce import brute_force
@@ -21,12 +20,8 @@ def main():
             p2 = int(point.strip().split(' ')[1])
             plist.append((p1, p2))
 
-    # Do all sorting work up front O(n) complexity
-    xsorted = sort_rands_on_x(plist)
-    ysorted = sort_rands_on_y(plist)
-
-    # closest-pair returns a distance object, class def found in Distance.py
-    final = closest_pair(xsorted, ysorted, distObj)
+    # closest-pair returns a distance object, class def. found in Distance.py
+    final = closest_pair(plist, distObj)
 
     print final.getDistance()
     for i in final.getPoints():
@@ -34,14 +29,17 @@ def main():
     return
 
 
-def closest_cross_pair(xlist, ylist, delta):
+def closest_cross_pair(xlist, delta):
     distObj = Distance.Distance()
     length = int(len(xlist))
     mid = xlist[length//2][0]
 
+    # sort y points in each iteration
+    ysorted = sort_rands_on_y(xlist)
+
     # strip is a list of y sorted points built by finding all x-coords that exist
     # within the boundries of the strip, ie mid - delta and mid + delta
-    strip = [x for x in ylist if mid - delta <= x[0] <= mid + delta]
+    strip = [x for x in ysorted if mid - delta <= x[0] <= mid + delta]
 
     index = 0
     # constant 7*n time
@@ -54,36 +52,29 @@ def closest_cross_pair(xlist, ylist, delta):
     return distObj
 
 # Recursive function closest_pair()
-def closest_pair(xlist, ylist, distObj):
-    length = len(xlist)
+def closest_pair(plist, distObj):
+    # sort list by x coord in each iteration
+    xsorted = sort_rands_on_x(plist)
+    length = len(xsorted)
     index = 0
 
     # if length of x-sorted array has 3 or less points, then brute-force compare
     # the points and return the min distance/points as a Distance object
     if(length <= 3):
-        for point in xlist:
-            if index + 1 < len(xlist):
-                distObj.safeAddPoints(point, xlist[index+1])
+        for point in xsorted:
+            if index + 1 < len(xsorted):
+                distObj.safeAddPoints(point, xsorted[index+1])
             index += 1
         return distObj
 
     mid = length // 2
-    leftArrayX = xlist[:mid]
-    rightArrayX = xlist[mid:]
-    leftArrayY = []
-    rightArrayY = []
-
-    # build to left/right arrays from the ysorted list
-    for point in ylist:
-        if point[0] <= xlist[mid][0]:
-            leftArrayY.append(point)
-        else:
-            rightArrayY.append(point)
+    leftArrayX = xsorted[:mid]
+    rightArrayX = xsorted[mid:]
 
     # Recursively call closest_pair on each half of the x-sorted array,
     # stopping when base case length<=3 is achieved
-    distObj1 = closest_pair(leftArrayX, leftArrayY, distObj)
-    distObj2 = closest_pair(rightArrayX, rightArrayY, distObj)
+    distObj1 = closest_pair(leftArrayX, distObj)
+    distObj2 = closest_pair(rightArrayX, distObj)
     distObj1.combine(distObj2)
 
     # determine delta by gettng the smallest distance returned by the recursion
@@ -91,7 +82,7 @@ def closest_pair(xlist, ylist, distObj):
 
     # call constant time closest_cross_pair() to determine the smallest distance
     # in the middle strip between the left and right x-sorted arrays
-    min_cross = closest_cross_pair(xlist, ylist, delta)
+    min_cross = closest_cross_pair(xsorted, delta)
 
     # return the smallest distance of all 3 options
     return distObj1.combine(min_cross)
--- run-brute-force.py
+++ run-brute-force.py
@@ -13,18 +13,14 @@ def main():
     filename = sys.argv[1]
     distObj = Distance.Distance()
     plist = []
-    minlist = []
 
     with open(filename, 'r') as file:
-        # p1 is x coord, p2 is y coord
         for point in file:
             p1 = int(point.strip().split(' ')[0])
             p2 = int(point.strip().split(' ')[1])
             plist.append((p1, p2))
 
     xsorted = sort_rands_on_x(plist)
-
-    #final = closest_pair(xsorted, distObj)
 
     final = brute_force(plist, distObj)
 
